bcdbd9627c2a207ff80f301f26ed6950f70cbe6c
Tony <t2ly@ucsd.edu>
2016-02-10 12:19:33 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,17 +5,6 @@ $(document).ready(function() {
 	initializePage();
 });
 
-// Credit: http://stackoverflow.com/a/2648463
-String.prototype.format = String.prototype.f = function() {
-	var s = this,
-		i = arguments.length;
-
-	while (i--) {
-		s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
-	}
-	return s;
-};
-
 /*
  * Function that is called when the document is ready.
  */
@@ -26,22 +15,17 @@ function initializePage() {
 }
 
 function createProjectDetails($details, data) {
-	var html = '<h2>{0}</h2><div class="summary"><img src="{2}" class="detailsImage">{1}</div>'.f(data.title, data.summary, data.image);
-	$details.html(html);
+	var detail = '<h2>{0}</h2><div class="summary"><img src="{2}" class="detailsImage">{1}</div>'.f(data.title, data.summary, data.image);
+	$details.html(detail);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
 	e.preventDefault();
-
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	var projectID = $(this).closest('.project').attr('id');
 	var $details = $(this).siblings('.details');
 	console.log($details);
 
@@ -65,3 +49,13 @@ function randomizeColors(e) {
 		$('.project img').css('opacity', .75);
 	})
 }
+
+String.prototype.format = String.prototype.f = function() {
+	var s = this,
+		i = arguments.length;
+
+	while (i--) {
+		s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+	}
+	return s;
+};
